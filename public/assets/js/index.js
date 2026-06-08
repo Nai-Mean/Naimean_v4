d306c91af4a6e784b4130b878af3a8d249f0b0b1
Matteo <naimean@gmail.com>
2026-06-08 17:03:12 -0400

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,35 +1,44 @@
 // public/assets/js/index.js
 
-// Core
+// ------------------------------------------------------
+// CORE
+// ------------------------------------------------------
 import { initDomRefs } from './core/domRefs.js';
 
-// Systems
+// ------------------------------------------------------
+// SYSTEMS
+// ------------------------------------------------------
 import { initMonitors } from './systems/monitors.js';
 import { initCornerScore } from './systems/cornerScore.js';
+import { initDvd } from './systems/dvd.js';
+import { initAquarium } from './systems/aquarium.js';
+import { initPrompt } from './systems/prompt.js';
+import { initLogin } from './systems/login.js';
+import { initTools } from './systems/tools.js';
+import { initHotspots } from './systems/hotspots.js';
 
-// As we build more systems, you will uncomment/add these:
-// import { initDvd } from './systems/dvd.js';
-// import { initAquarium } from './systems/aquarium.js';
-// import { initPrompt } from './systems/prompt.js';
-// import { initLogin } from './systems/login.js';
-// import { initTools } from './systems/tools.js';
-// import { initHotspots } from './systems/hotspots.js';
-// import { initPerformance } from './ui/performance.js';
+// ------------------------------------------------------
+// UI / PERFORMANCE
+// ------------------------------------------------------
+import { initPerformance } from './ui/performance.js';
 
+// ------------------------------------------------------
+// BOOTSTRAP
+// ------------------------------------------------------
 window.addEventListener('DOMContentLoaded', () => {
-  // Core DOM references
-  initDomRefs();
+    // Core DOM references
+    initDomRefs();
 
-  // Systems
-  initMonitors();
-  initCornerScore();
+    // Systems
+    initMonitors();
+    initCornerScore();
+    initDvd();
+    initAquarium();
+    initPrompt();
+    initLogin();
+    initTools();
+    initHotspots();
 
-  // Later, when modules are ready:
-  // initDvd();
-  // initAquarium();
-  // initPrompt();
-  // initLogin();
-  // initTools();
-  // initHotspots();
-  // initPerformance();
+    // UI
+    initPerformance();
 });
